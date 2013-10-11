2beb79cd33b6aed26a2a49193eedc8df6192e163
Mike Paßberg <mike.passberg@zanox.com>
2013-10-11 11:48:23 +0200

    Call encode on object not from class

--- net.py
+++ net.py
@@ -3,12 +3,11 @@ import socket
 
 #graphite send_data
 class graphite(object):
-
     __graphite_server = "localhost"
     __graphite_port = 2003
     __soc = None
 
-    def __init__(self,port=2003,server="127.0.0.1"):
+    def __init__(self, port=2003, server="127.0.0.1"):
         self.__graphite_port = port
         self.__graphite_server = server
         self.__soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,16 +21,16 @@ class graphite(object):
         self.__soc.close()
 
     def connect(self):
-        self.__soc.connect( (self.__graphite_server,self.__graphite_port) )
+        self.__soc.connect((self.__graphite_server, self.__graphite_port))
 
     def close(self):
         self.__soc.shutdown(socket.SHUT_RDWR)
         self.__soc.close()
 
-    def send_data(self,path,value,timestamp):
+    def send_data(self, path, value, timestamp):
         self.__soc
 
         #print "%s %s %s" % (path,value,timestamp)
-        self.__soc.sendall(str.encode("%s %s %s\n" % (path,value,timestamp)) )
+        self.__soc.sendall(("%s %s %s\n" % (path, value, timestamp)).encode())
         #__soc.flush()
         #__soc.close()
--- test.py
+++ test.py
@@ -3,11 +3,12 @@ __author__ = 'suxor'
 import net
 from mock import Mock
 
+
 def test_graphite():
     net.graphite.connect = Mock()
     net.graphite.send_data = Mock(return_value=None)
     net.graphite.__exit__ = Mock()
 
     with net.graphite(server="localhost") as graphite:
-        result = graphite.send_data("test",10000,10000)
-        assert(result is None)
+        result = graphite.send_data("test", 10000, 10000)
+        assert (result is None)
