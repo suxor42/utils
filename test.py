a43a316f09865a91ab9e64df360c0ad3c8cb864e
Mike Paßberg <mike.passberg@zanox.com>
2014-01-28 11:40:48 +0100

    Add col module Trying autodict.

--- test.py
+++ test.py
@@ -1,14 +1,36 @@
 __author__ = 'suxor'
 
 import net
-from mock import Mock
+#from mock import Mock
+import col
+import collections
 
 
-def test_graphite():
-    net.Graphite.connect = Mock()
-    net.Graphite.send_data = Mock(return_value=None)
-    net.Graphite.__exit__ = Mock()
+#def test_graphite():
+#    net.Graphite.connect = Mock()
+#    net.Graphite.send_data = Mock(return_value=None)
+#    net.Graphite.__exit__ = Mock()
+#
+#    with net.Graphite(server="localhost") as graphite:
+#        result = graphite.send_data("test", 10000, 10000)
+#        assert (result is None)
 
-    with net.Graphite(server="localhost") as graphite:
-        result = graphite.send_data("test", 10000, 10000)
-        assert (result is None)
+
+dict = col.autodict()
+#dict['lala']['blub'] = dict()
+#dict['lala']['blub']['lala']['bla'] = 1
+#test = dict['lala']['123']
+#print(dict)
+#print('123' in dict['lala'])
+
+dict = {}
+dict['1'] = 6
+dict['bla'] = {}
+dict = col.autodict(dict)
+#dict = col.autodict(map(dict.transform, dict.traverse(dict)))
+print dict
+dict['lala']['blub']['5'] = 4
+dict['bla']['lala']['4'] = 5
+print dict
+#for v in dict.traverse(dict):
+#    print v
